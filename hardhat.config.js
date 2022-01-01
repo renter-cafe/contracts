c0b8313c893848bee46562afe01aa8b9bff00168
Philippe Schommers <philippe@schommers.be>
2022-01-01 19:28:36 +0100

    Fix hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,10 @@
+require('dotenv').config()
+
+require('@nomiclabs/hardhat-etherscan')
+require('@nomiclabs/hardhat-waffle')
+require('hardhat-gas-reporter')
+require('solidity-coverage')
+
 module.exports = {
 	solidity: {
 		version: '0.8.9',
